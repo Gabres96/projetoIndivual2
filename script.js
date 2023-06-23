476e80abb511d7e76cb9aeeec6d0de66e0f2cb69
Gabres96 <gabriel.rodrigues670@gmail.com>
2023-06-22 23:42:06 -0300

    Add tipo de conversor e melhoria nos coments

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 var valor = document.getElementById('valor');
-
 var txtResultado = document.getElementById('resultado')
+var tipoConversao = document.getElementById("tipoConversao")
 
-unidade1 = document.getElementById('unidade1');
-unidade1.addEventListener('change', converter);
+seletorDe = document.getElementById('seletorDe');
+seletorDe.addEventListener('change', converter);
 
-unidade2 = document.getElementById('unidade2');
-unidade2.addEventListener('change', converter);
+seletorPara = document.getElementById('seletorPara');
+seletorPara.addEventListener('change', converter);
 
 var resultado;
 
@@ -18,47 +18,47 @@ function converter() {
     numero = parseFloat(numero);
 
     /*---CALCULO METRO--*/
-    if (unidade1.value == 'metro' && unidade2.value == 'metro') {
+    if (seletorDe.value == 'metro' && seletorPara.value == 'metro') {
         resultado = numero * 1;
         txtResultado.innerHTML = `${numero} metro(s) equivalem a ${resultado} metro(s)`
     }
 
-    if (unidade1.value == 'metro' && unidade2.value == 'centimetro') {
+    if (seletorDe.value == 'metro' && seletorPara.value == 'centimetro') {
         resultado = numero * 100;
         txtResultado.innerHTML = `${numero} metro(s) equivalem a ${resultado} centimetro(s)`
     }
 
-    if (unidade1.value == 'metro' && unidade2.value == 'polegada') {
+    if (seletorDe.value == 'metro' && seletorPara.value == 'polegada') {
         resultado = numero * 39.37;
         txtResultado.innerHTML = `${numero} metro(s) equivalem a ${resultado} polegada(s)`
     }
     /*---CALCULO CENTIMETRO--*/
-    if (unidade1.value == 'centimetro' && unidade2.value == 'metro') {
+    if (seletorDe.value == 'centimetro' && seletorPara.value == 'metro') {
         resultado = numero * 0.01;
         txtResultado.innerHTML = `${numero} centimetro(s) equivalem a ${resultado} metro(s)`
     }
 
-    if (unidade1.value == 'centimetro' && unidade2.value == 'centimetro') {
+    if (seletorDe.value == 'centimetro' && seletorPara.value == 'centimetro') {
         resultado = numero * 1;
         txtResultado.innerHTML = `${numero} centimetro(s) equivalem a ${resultado} centimetro(s)`
     }
 
-    if (unidade1.value == 'centimetro' && unidade2.value == 'polegada') {
+    if (seletorDe.value == 'centimetro' && seletorPara.value == 'polegada') {
         resultado = numero * 0.39;
         txtResultado.innerHTML = `${numero} metro(s) equivalem a ${resultado} polegada(s)`
     }
     /*---CALCULO POLEGADA--*/
-    if (unidade1.value == 'polegada' && unidade2.value == 'metro') {
+    if (seletorDe.value == 'polegada' && seletorPara.value == 'metro') {
         resultado = numero / 39.37;
         txtResultado.innerHTML = `${numero} polegada(s) equivalem a ${resultado} metro(s)`
     }
 
-    if (unidade1.value == 'polegada' && unidade2.value == 'centimetro') {
+    if (seletorDe.value == 'polegada' && seletorPara.value == 'centimetro') {
         resultado = numero * 2.54;
         txtResultado.innerHTML = `${numero} metro(s) equivalem a ${resultado} centimetro(s)`
     }
 
-    if (unidade1.value == 'polegada' && unidade2.value == 'polegada') {
+    if (seletorDe.value == 'polegada' && seletorPara.value == 'polegada') {
         resultado = numero * 1;
         txtResultado.innerHTML = `${numero} metro(s) equivalem a ${resultado} polegada(s)`
     }
