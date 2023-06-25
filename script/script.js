8e8d38ac2e0d5d67e6324ea2a0e2faeb27eb051b
Gabres96 <gabriel.rodrigues670@gmail.com>
2023-06-25 18:39:51 -0300

    html e logica funcionando + elementos css

--- script/script.js
+++ script/script.js
@@ -1,120 +1,70 @@
-var escolherMedida = document.getElementById("seletorMedida");
-var escolherOpcaoDe = document.getElementById("opcaoDe");
-var escolherOpcaoPara = document.getElementById("opcaoPara");
-var selecionar = document.getElementById("selecionar");
+document.getElementById("formularioConversor").addEventListener("submit", function (event) {
+    event.preventDefault()
 
-escolherMedida.addEventListener("change", function () {
-    var seletorMedida = escolherMedida.value;
+    var valorDe = document.getElementById("valorDe")
+    var valorPara = document.getElementById("valorPara")
+    var valor = document.getElementById("valor")
 
-    if (seletorMedida === "comprimento") {
-        populateOptions(["Metros", "Centímetros", "Polegadas"]);
-    } else if (seletorMedida === 'peso') {
-        populateOptions(["Quilogramas", "Gramas", "Libras"]);
-    } else if (seletorMedida === 'temperatura') {
-        populateOptions(["Celsius", "Fahrenheit", "Kelvin"]);
-    } else if (seletorMedida === 'escolha') {
-        populateOptions(['Selecione uma categoria'])
-    }
-});
+    var resultado = calculoConversoes(valorDe.value, valorPara.value, valor.value)
 
-function populateOptions(options) {
-    escolherOpcaoDe.innerHTML = '';
-    escolherOpcaoPara.innerHTML = '';
-
-    options.forEach(function (option) {
-        var optionElement = document.createElement('option');
-        optionElement.value = option;
-        optionElement.textContent = option;
-
-        opcaoDe.appendChild(optionElement);
-        opcaoPara.appendChild(optionElement.cloneNode(true));
-    });
-}
-
-var criarBotaoConverter = document.querySelector(".botaoConverter");
-
-criarBotaoConverter.addEventListener("click", function () {
-    var valor = document.getElementById("valor").value;
-    var seletorMedida = escolherMedida.value;
-    var opcaoDe = escolherOpcaoDe.value;
-    var opcaoPara = escolherOpcaoPara.value;
-
-    if (valor === "") {
-        alert(document.getElementById("campoResultado").value = "Digite algum valor para ser calculado.")
+    var valorFinal = document.getElementById("valorFinal")
+    if (valorDe.selectedIndex === 0 || valorPara.selectedIndex === 0) {
+        alert("Algum campo não foi preenchido, por gentileza corrija")
     } else {
-        if (seletorMedida === "" || opcaoDe === "" || opcaoPara === "") {
-            alert(document.getElementById("campoResultado").value = "Adicione informações em todos os campos")
-        } else {
-            var resultado = realizarConversao(valor, seletorMedida, seletorOrigem, seletorDestino);
-
-            Document.getElementById("campoResultado").value = resultado;
-
-        }
-    }
-
-});
-
-document.addEventListener("keydown", function (event) {
-    if (event.key === "enter") {
-        criarBotaoConverter.click();
+        valorFinal.style.color = "white"
+        valorFinal.innerText = resultado.toFixed(2)
     }
 })
+document.getElementById("escolherMedidas").addEventListener("change", function (event) {
+    event.preventDefault()
 
-function realizarConversao(valor, seletorMedida, opcaoDe, opcaoPara) {
-    if (opcaoDe === opcaoPara) {
-        return parseFloat(valor);
+    var definir = event.target.value
+
+    var lista = []
+    if (definir == "Comprimento") { lista = ["Metros", "Centimetros", "Polegadas"] }
+    if (definir == "Peso") { lista = ["Quilogramas", "Gramas", "Libras"] }
+    if (definir == "Temperatura") { lista = ["Celsius", "Fahrenheit", "Kelvin"] }
+    opcoesMedidas(lista)
+})
+
+function opcoesMedidas(lista) {
+    valorDe.innerHTML = "<option disabled selected hidden>Qual medida você deseja?</option>"
+    for (var i = 0; i < lista.length; i++) {
+        var opcao = document.createElement("option")
+        opcao.innerText = `${lista[i]}`
+        valorDe.append(opcao)
     }
+    document.getElementById("valorDe").addEventListener("change", function (event) {
+        event.preventDefault()
 
-    if (seletorMedida === "comprimento") {
-        if (opcaoDe === "Metros" && opcaoPara === 'Centímetros') {
-            return (valor * 100).toFixed(2);
-        } else if (opcaoDe === "Centímetros" && opcaoPara === "Metros") {
-            return (valor / 100).toFixed(2);
-        } else if (opcaoDe === "Polegadas" && opcaoPara === "Centímetros") {
-            return (valor * 2.54).toFixed(2);
-        } else if (opcaoDe === "Metros" && opcaoPara === "Polegadas") {
-            return (valor / 2.54).toFixed(2);
+        var definir = event.target.selectedIndex
+        valorPara.innerHTML = "<option disabled selected hidden>Qual medida você deseja?</option>"
 
-        } else if (opcaoDe === "Metros" && opcaoPara === "Polegadas") {
-            return (valor * 39.37).toFixed(2);
-        } else if (opcaoDe === "Polegadas" && opcaoPara === "Metros") {
-            return (valor / 39.37).toFixed(2);
+        for (var i = 0; i < lista.length; i++) {
+            if (definir - 1 != i) {
+                var opcao = document.createElement("option")
+                opcao.innerText = `${lista[i]}`
+                valorPara.append(opcao)
+            }
         }
-    } else if (seletorMedida === "peso") {
-        if (opcaoDe === "Quilogramas" && opcaoPara === "Gramas") {
-            return (valor * 1000).toFixed(2);
-        } else if (opcaoDe === "Gramas" && opcaoPara === "Quilogramas") {
-            return (valor / 1000).toFixed(2);
-        } else if (opcaoDe === "Libras" && opcaoPara === "Quilogramas") {
-            return (valor * 0.453592).toFixed(2);
-        } else if (opcaoDe === "Quilogramas" && opcaoPara === "Libras") {
-            return (valor * 2.20462).toFixed(2);
-
-        } else if (opcaoDe === "Gramas" && opcaoPara === "Polegadas") {
-            return (valor / 453.592).toFixed(2);
-        } else if (opcaoDe === "Libras" && opcaoPara === "Gramas") {
-            return (valor * 453.592).toFixed(2);
-        }
-    } else if (seletorMedida === "temperatura") {
-        if (opcaoDe === "Celsius" && opcaoPara === "Fahrenheit") {
-            return ((valor * 9 / 5 + 32).toFixed(2));
-        } else if (opcaoDe === "Fahrenheit" && opcaoPara === "Celsius") {
-            return (valor / 1000).toFixed(2);
-        } else if (opcaoDe === "Celsius" && opcaoPara === "Kelvin") {
-            return (parseFloat(valor) + 273.15).toFixed(2);
-        } else if (opcaoDe === "Kelvin" && opcaoPara === "Celsius") {
-            return (parseFloat(valor) - 273.15).toFixed(2);
-        } else if (opcaoDe === "Fahrenheit" && opcaoPara === "Kelvin") {
-            return ((parseFloat(valor) + 459.67) * 5 / 9).toFixed(2);
-        } else if (opcaoDe === "Kelvin" && opcaoPara === "Fahrenheit") {
-            return ((parseFloat(valor) * 9 / 5) - 459.67).toFixed(2);
-        }
-    }
-    return "Conversão Inválida";
+    })
 }
 
+function calculoConversoes(valorDe, valorPara, valor) {
+
+    if (valorDe == "Quilogramas") { if (valorPara == "Gramas") { return valor * 1000 } if (valorPara == "Libras") { return valor * 2.20462 } }
+    if (valorDe == "Gramas") { if (valorPara == "Quilogramas") { return valor * 0.001 } if (valorPara == "Libras") { return valor * 0.00220462 } }
+    if (valorDe == "Libras") { if (valorPara == "Quilogramas") { return valor * 0.453592 } if (valorPara == "Gramas") { return valor * 453.5920000001679 } }
 
 
+    if (valorDe == "Celsius") { if (valorPara == "Fahrenheit") { return valor * 1.8 + 32 } if (valorPara == "Kelvin") { return valor * 1 + 273.15 } }
+    if (valorDe == "Fahrenheit") { if (valorPara == "Celsius") { return (valor - 32) / 1.8 } if (valorPara == "Kelvin") { return (valor - 32) * 5 / 9 + 273.15 } }
+    if (valorDe == "Kelvin") { if (valorPara == "Celsius") { return valor - 273.15 } if (valorPara == "Fahrenheit") { return (valor - 273.15) * 1.8 + 32 } }
+
+    if (valorDe == 'Metros') { if (valorPara == 'Centimetros') { return valor * 100 } if (valorPara == 'Polegadas') { return valor * 39.3701 } }
+    if (valorDe == 'Centimetros') { if (valorPara == 'Metros') { return valor * 0.01 } if (valorPara == 'Polegadas') { return valor * 0.393701 } }
+    if (valorDe == 'Polegadas') { if (valorPara == 'Metros') { return valor * 0.0254 } if (valorPara == 'Centimetros') { return valor * 2.54 } }
+}
 
 
 
